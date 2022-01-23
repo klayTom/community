a750ca5928b01afe8050c14af2840551b20c676c
klay <klay@jj.com>
2022-01-23 21:44:35 +0800

    完成SDK文件上传

--- src/main/java/com/klay/dto/FileDto.java
+++ src/main/java/com/klay/dto/FileDto.java
@@ -1,8 +1,12 @@
 package com.klay.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FileDto {
     private int success;
     private String message;
---
+++ src/main/java/com/klay/util/TencentCOSUtil.java
@@ -0,0 +1,9 @@
+package com.klay.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TencentCOSUtil {
+
+}
